6dcfa68392b48e94debd392f5cac806c50027e0b
Jason Watmore <cornflourblue@hotmail.com>
2022-12-27 16:42:15 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# angular-14-basic-authentication-example
+
+Angular 14 - Basic Authentication Tutorial with Example
+
+Documentation coming soon!!
---
+++ src/app/_models/user.ts
@@ -0,0 +1,8 @@
+﻿export interface User {
+    id: number;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    authdata?: string;
+}
